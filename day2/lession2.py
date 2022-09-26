cd5d0b922e2999c6dd74b7caf7d3c7b18e6d4508
eliashvili45 <eliashvili9442@gmail.com>
2022-09-26 05:07:35 -0700

    first add

---
+++ day2/lession2.py
@@ -0,0 +1,33 @@
+#full_name = "nika eliashvili"
+
+#print(full_name[2:7])
+
+
+#nika = "medusa"
+#print(nika)
+
+name = "nika"
+age = 27
+height = 174
+ 
+print("chemi saxelia {} chemi asakia {} simagle {}".format(name, age, height))
+
+
+full_name = "nika eliashvili"
+
+print(full_name[2:8])
+
+print(len(full_name))
+
+if "e" in full_name:
+
+    print("hallo")
+    print(full_name[::2])
+
+    print(full_name.upper())
+
+    name = "   nika  "
+
+    print(name.strip())
+
+    print(name.replace("n","l"))
